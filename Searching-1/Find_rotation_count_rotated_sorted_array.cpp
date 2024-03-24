dd0468a2fc85b913218e43cd9c18a3a571dc5cc3
harshraj2717 <harshraj2717@gmail.com>
2024-03-24 10:16:50 +0530

    add prev ques + new ones

---
+++ Searching-1/Find_rotation_count_rotated_sorted_array.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool ispossible(int d, int s, vector<int> &st)
+{
+    for (int i = 0; i < st.size(); i += d)
+    {
+        int j = i;
+        int sum = 0;
+        while (j < st.size() && j < i + d)
+            sum += st[j++];
+        if (sum > s)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    // your code goes here
+    int n, s;
+    cin >> n >> s;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int l = 1, h = n, ans = -1;
+    while (l <= h)
+    {
+        int m = l + (h - l) / 2;
+        if (ispossible(m, s, a))
+        {
+            ans = m;
+            l = m + 1;
+        }
+        else
+            h = m - 1;
+    }
+    cout << ans << endl;
+    return 0;
+}
---
+++ searching-3/Maximum_Lunch_Box.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<int, int> mp;
+
+bool ispossible(int m, int a)
+{
+	int c = 0;
+	for (auto it : mp)
+	{
+		int f = (it.second / m);
+		c += f;
+		if (c >= a)
+			return true;
+	}
+	return false;
+}
+
+int main()
+{
+	// your code goes here
+	int n;
+	cin >> n;
+	vector<int> b(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> b[i];
+		mp[b[i]]++;
+	}
+	int a;
+	cin >> a;
+	int l = 1, h = b.size() / a, ans = -1;
+	while (l <= h)
+	{
+		int m = l + (h - l) / 2;
+		if (ispossible(m, a))
+		{
+			ans = m;
+			l = m + 1;
+		}
+		else
+			h = m - 1;
+	}
+	cout << ans << endl;
+	return 0;
+}
